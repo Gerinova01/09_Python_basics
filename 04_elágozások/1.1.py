b3c575ec0cdb0cada3a2a7f1451d1466520f60e8
Kullai Gergő <KullaiGergo@pg.local>
2025-09-24 09:57:45 +0200

    Create 1.1.py

---
+++ 04_elágozások/1.1.py
@@ -0,0 +1,13 @@
+# szam = 1
+# while szam <= 5:
+#     print(szam *' * ')
+#     szam = szam +1
+
+szam = 1
+while szam <=5:
+    print(szam *' * ')
+    szam = szam +1
+
+while szam >= 1:
+    print(szam * ' * ')
+    szam = szam -1

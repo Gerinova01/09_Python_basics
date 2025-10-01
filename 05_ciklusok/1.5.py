c33cfd4e1c6147173e34abbebf999449e92a085b
Gerinova01 <kullai.gergo@diak.szbi-pg.hu>
2025-10-01 11:58:01 +0200

    Create 1.5.py

---
+++ 05_ciklusok/1.5.py
@@ -0,0 +1,10 @@
+"""
+    1. Összegszámítás
+Kérj be egy egész számot (pl. 10; 13;  20…),
+és számítsd ki az 1-től a megadott számig terjedő egész számok összegét.
+"""
+
+szam = int(input("Adj meg egy egész számot számot "))
+
+for x in range(1, szam):
+    print(x)
